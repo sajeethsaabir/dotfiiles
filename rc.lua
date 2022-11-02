cf361439893e1f2d2b6e6430fb45a5b2cb2daae6
Zukxxxx0 <sajiiahd@gmail.com>
2022-11-02 14:59:44 +0530

    fix: remove, added battery logo configuration

--- rc.lua
+++ rc.lua
@@ -514,32 +514,3 @@ end)
 awful.spawn.with_shell("picom")
 awful.spawn.with_shell("nitrogen --set-zoom-fill --restore")
 
--- battery warning
--- From a blog post
-
-local function trim(s)
-	  return s:find'^%s*$' and '' or s:match'^%s*(.*%S)'
-  end
-
-  local function bat_notification()
-	    
-	    local f_capacity = assert(io.open("/sys/class/power_supply/BAT0/capacity", "r"))
-	      local f_status = assert(io.open("/sys/class/power_supply/BAT0/status", "r"))
-
-	        local bat_capacity = tonumber(f_capacity:read("*all"))
-		  local bat_status = trim(f_status:read("*all"))
-
-		    if (bat_capacity <= 30 and bat_status == "Discharging") then
-			        naughty.notify({ title      = "Battery Warning"
-				      , text       = "Battery low! " .. bat_capacity .."%" .. " left!"
-				            , fg="#ff0000"
-					          , bg="#deb887"
-						        , timeout    = 15
-							      , position   = "bottom_left"
-							          })
-								    end
-							    end
-
-							    battimer = timer({timeout = 120})
-							    battimer:connect_signal("timeout", bat_notification)
-							    battimer:start()
