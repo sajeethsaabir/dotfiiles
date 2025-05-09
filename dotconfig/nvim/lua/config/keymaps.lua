603e12bc9cfc46f9527b947d5c6fe11c3ce041a1
sajeethsaabir <sajiiahd@gmail.com>
2025-05-09 15:33:16 +0530

    feat: new neovim config folder

---
+++ dotconfig/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
+vim.keymap.set("n", "gl", function()
+    vim.diagnostic.open_float()
+end, { desc = "Open Diagnostics in Float" })
+
+vim.keymap.set("n", "<leader>cf", function()
+    require("conform").format({
+        lsp_format = "fallback",
+    })
+end, { desc = "Format current file" })
+
+-- Map <leader>fp to open projects
+vim.keymap.set("n", "<leader>fp", ":ProjectFzf<CR>", { noremap = true, silent = true })
